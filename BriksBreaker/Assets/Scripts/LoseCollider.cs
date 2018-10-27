5c0efe7b0a2f6fa4e069c02e1a896200e3a22047
Jornau <alastarsurkov@gmail.com>
2018-10-27 15:43:45 +0300

    stage 1

---
+++ BriksBreaker/Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoseCollider : MonoBehaviour {
+
+    [SerializeField] GameObject iFace;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Ball")
+        {
+            iFace.SetActive(true);
+        }
+    }
+
+    public void RestartLevel()
+    {
+        iFace.SetActive(false);
+        SceneManager.LoadScene("SampleScene");
+    }
+}
